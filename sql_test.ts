35dcbac447a60b0e222d81be65766f77b8c42f90
Bruno <brmcerqueira@gmail.com>
2020-10-18 17:10:46 -0300

    Adjust

--- sql_test.ts
+++ sql_test.ts
@@ -1,11 +1,16 @@
 import { assert, assertEquals, assertStrictEquals, assertThrows } from "https://deno.land/std/testing/asserts.ts";
-import { raw, sql } from "./sql.ts";
+import { raw, sql, where } from "./sql.ts";
 
 Deno.test("select", () => {  
     let firstName = 22;
     let lastName = "Tendulkar";
       
-    console.log(sql`select * from User where 
-    firstName = ${firstName} and lastName =
-    ${raw(lastName)}`);
+    console.log(sql`select * from User ${where(w => {
+        if (firstName > 10) {
+            w.and`firstName = ${firstName}`;
+        }
+        if (lastName == "Tendulkar") {
+            w.and`lastName = ${raw(lastName)}`;
+        }
+    })}`);
 });
